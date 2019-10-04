6111a112dcc49de898e74e0408b2cc61ce2f34b2
Tyler Lawson <tylerlawson12@live.com>
2019-10-04 15:02:39 -0700

    finished question 1

--- 201D53/class-05/lab-a/starter-code/app.js
+++ 201D53/class-05/lab-a/starter-code/app.js
@@ -9,7 +9,10 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
+var addSum = a + b;
+var answer = 'The sum of ' + a + ' and ' + b + ' is ' + addSum + '.';
 
+return [addSum, answer];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -28,6 +31,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
 
+  return a * b;
 }
 
 // Here is the test for multiply(); uncomment it to run it
